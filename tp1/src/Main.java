374fe712049851247026debe6eee05d8549cf4a0
marcoscassone02 <ma.cassone@alumno.um.edu.ar>
2025-04-09 04:10:44 -0300

    arraylist en main

--- tp1/src/Main.java
+++ tp1/src/Main.java
@@ -1,27 +1,22 @@
 package programacion_2_trabajo_practico_1_marcoscassone02.tp1.src;
+import java.util.ArrayList;
+
 
 public class Main {
-    public Main() {
-    }
+    public static void main(String[] args) {
+        ArrayList<Vehiculo> vehiculos = new ArrayList<>();
 
-    public static void main(String[] var0) {
-        Vehiculo var1 = new Vehiculo("ABC123", "Ford", 2020, 1500.0);
-        Vehiculo var2 = new Vehiculo("XYZ789", "Chevrolet", 2021, 1200.0);
-        Vehiculo var3 = new Vehiculo("LMN456", "Toyota", 2022, 1000.0);
-        Vehiculo var4 = new Camion("CAM456", "Volvo", 2018, 5000.0, true);
-        Vehiculo var5 = new Auto("GHD326", "Fiat", 2010, 800, 5);
+        vehiculos.add(new Auto("ABC123", "Toyota", 2023, 1500.0, 5));
+        vehiculos.add(new Camion("DEF456", "Volvo", 2020, 5000.0, true));
+        vehiculos.add(new Auto("GHI789", "Ford", 2022, 1300.0, 4));
 
         VehiculoPrinter printer = new VehiculoPrinter();
 
-        printer.imprimirInformacion(var1);
-        System.out.println();
-        printer.imprimirInformacion(var2);
-        System.out.println();
-        printer.imprimirInformacion(var3);
-        System.out.println();
-        printer.imprimirInformacion(var4);
-        System.out.println();
-        printer.imprimirInformacion(var5);
+        for (Vehiculo v : vehiculos) {
+            printer.imprimirInformacion(v);
+            System.out.println();
+        }
     }
 }
+
 
